19fd84c13b19a829172a765ab44e898610fed02c
Yousuf Borno <yousuf@gmail.com>
2024-03-31 09:51:07 -0700

    products model created

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,14 @@
 const mongoose = require("mongoose");
 
 //HOW TO CREATE A TABLE IN MONGODB
-const ProductSchema = mongoose.Schema({});
+const ProductSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
