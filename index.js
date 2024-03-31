a85420f11290595d9c9457a19753ac8060ffc225
Yousuf Borno <yousuf@gmail.com>
2024-03-31 11:51:02 -0700

    MongoDB create and get all information

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+//IMPORTING THE MODEL THAT WE HAVE CREATED
+const Product = require("./models/product.model");
 const app = express();
 
 function middleWareFunction(req, res) {
   res.send("<h1>Hello this is the first middleware</h1>");
 }
 
+//use to sen body in post
+app.use(express.json());
+
 app.get("/", middleWareFunction);
+
+//FOR GETTING ALL RECORDS FROM MONGODB
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//FOR GETTING INSERTING RECORDS IN MONGODB
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 /*
 app.listen(3000, () => {
   console.log("This app is listening to 3000, localhost:3000/");
 });
 */
+
 mongoose
   .connect(
     "mongodb+srv://yousufpractice:Ur0rqUxzAJLw8mqC@newone.l1eytth.mongodb.net/{newOne}?retryWrites=true&w=majority&appName=newOne"
--- models/product.model.js
+++ models/product.model.js
@@ -11,4 +11,18 @@ const ProductSchema = mongoose.Schema({
     required: true,
     default: 0,
   },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
 });
+
+//This will help create the schema in the Mongo Cloud
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
